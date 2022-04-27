390d829ca4bfd8c167e63e860dc56e5ee6a057dc
hmoawiya <hmoawiya@drivenets.com>
2022-04-27 10:49:11 +0300

    added new ncc auto03

--- InstallNewDNOR.py
+++ InstallNewDNOR.py
@@ -14,7 +14,7 @@ import yaml
 versionLink = 'http://minioio.dev.drivenets.net:9000/dnor/comet-dnor-rel-14.2.1/dnor_release.14.2.1.4-bb07716adc.tar'
 dnorVersion = "V14"
 gdnor = False
-config = Config(dnor='dn36')
+config = Config(dnor='dn3643')
 
 def test01_Validate_prerequisites_VMs_are_up_and_reachable_Primary_VM():
     DNORFunctions.validate_prerequisites_VMs_are_up_and_reachable(config.primaryDNOR,config)
